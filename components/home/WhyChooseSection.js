a0a3cfd39ec821846355dd84e8cae70bdb01d011
gsauarv <gsaurav2000@gmail.com>
2024-05-27 13:07:23 +0545

    DESIGN:  added why choose use card u

--- components/home/WhyChooseSection.js
+++ components/home/WhyChooseSection.js
@@ -1,5 +1,10 @@
 import why1 from "@/public/why1.svg";
-import { Box, Button, Flex, Grid, GridItem, Text } from "@chakra-ui/react";
+import why2 from "@/public/why2.svg";
+import why3 from "@/public/why3.svg";
+import why4 from "@/public/why4.svg";
+import why5 from "@/public/why5.svg";
+
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
 import Image from "next/image";
 
 const WhyChooseSection = () => {
@@ -47,32 +52,66 @@ const WhyChooseSection = () => {
       </Flex>
 
       <Flex justifyContent={"center"}>
-        <Box sx={{ width: "1085px" }}>
-          <Grid
-            templateRows="repeat(4, 1fr)"
-            templateColumns="repeat(4, 1fr)"
-            gap={"20px"}
-            sx={{ paddingTop: "54px" }}
+        <Box sx={{ paddingBlock: "54px" }}>
+          <Flex
+            sx={{
+              alignItems: "center",
+              columnGap: "26px",
+              justifyContent: "center",
+            }}
           >
-            <GridItem colSpan={2}>
-              <WhyChooseUsCard />
-            </GridItem>
-            <GridItem colSpan={2}>
-              <WhyChooseUsCard />
-            </GridItem>
-            <GridItem colSpan={1}>
-              <WhyChooseUsCard />
-            </GridItem>{" "}
-            <GridItem>
-              <WhyChooseUsCard />
-            </GridItem>{" "}
-            <GridItem>
-              <WhyChooseUsCard />
-            </GridItem>{" "}
-            <GridItem>
-              <WhyChooseUsCard />
-            </GridItem>
-          </Grid>
+            <WhyChooseUsCard
+              image={why1}
+              title={"Accessibility and Convenience"}
+              description={
+                "Allows clients to access their accounting data from anywhere with an internet connection"
+              }
+            />
+
+            <WhyChooseUsCard
+              image={why2}
+              title={"Real-time Collaboration"}
+              description={
+                "Facilitate seamless collaboration for distributed teams and external accountants"
+              }
+              type="md"
+            />
+          </Flex>
+
+          <Flex
+            sx={{
+              columnGap: "26px",
+              justifyContent: "center",
+              paddingTop: "30px",
+            }}
+          >
+            <WhyChooseUsCard
+              image={why3}
+              title={"Automatic Updates and Maintenance"}
+              description={
+                "Enjoy automated updates & maintenance-free software."
+              }
+              type="sm"
+            />
+
+            <WhyChooseUsCard
+              image={why4}
+              title={"Data Security and Backup"}
+              description={
+                "Cloud-based accounting offers strong security features, such as data encryption, controlled access, & consistent backups."
+              }
+              type="sm"
+            />
+
+            <WhyChooseUsCard
+              image={why5}
+              title={"Reduced IT Overhead"}
+              description={
+                "Reduces the need for onsite servers & IT upkeep as cloud providers manage updates & maintenance."
+              }
+              type="sm"
+            />
+          </Flex>
         </Box>
       </Flex>
     </Box>
@@ -81,7 +120,35 @@ const WhyChooseSection = () => {
 
 export default WhyChooseSection;
 
-const WhyChooseUsCard = () => {
+const CardHeader = ({ title, description }) => {
+  return (
+    <>
+      <Box
+        sx={{
+          paddingTop: "42px",
+        }}
+      >
+        <Text sx={{ fontSize: "20px", color: "#201F37", fontWeight: 700 }}>
+          {title}
+        </Text>
+        <Text
+          sx={{
+            color: "#6C6B80",
+            fontSize: "14px",
+            fontWeight: 500,
+            textWrap: "wrap",
+            // width: "400px",
+            paddingTop: "5px",
+          }}
+        >
+          {description}
+        </Text>
+      </Box>
+    </>
+  );
+};
+
+const WhyChooseUsCard = ({ image, title, description, type = "large" }) => {
   return (
     <>
       <Box
@@ -89,30 +156,23 @@ const WhyChooseUsCard = () => {
           backgroundColor: "#F8FAFC",
           //   height: "429px",
           padding: "0px 31px",
-          borderRadius: "4px",
-          overflow: "hidden",
+          borderRadius: "6px",
+          width: "fit-content",
+          // overflow: "hidden",
+          width: type === "large" ? "640px" : type !== "sm" ? "419px" : "343px",
         }}
       >
-        <Box
-          sx={{
-            paddingTop: "42px",
-          }}
-        >
-          <Text sx={{ fontSize: "20px", color: "#201F37", fontWeight: 700 }}>
-            Accessibility and Convenience
-          </Text>
-          <Text sx={{ color: "#6C6B80", fontSize: "14px", fontWeight: 500 }}>
-            Allows clients to access their accounting data from anywhere with an
-            internet connection.
-          </Text>
-        </Box>
+        <CardHeader title={title} description={description} />
 
-        <Flex justifyContent={"flex-start"} sx={{ paddingTop: "36px" }}>
+        <Flex
+          justifyContent={"flex-start"}
+          sx={{ paddingTop: "36px", width: "fit-content" }}
+        >
           <Image
-            src={why1}
-            layout="responsive"
-            height={"286px"}
-            // width={"100%"}
+            src={image}
+            // layout="responsive"
+            // height={"286px"}
+            width={"100%"}
             objectFit="cover"
             objectPosition="center"
           />
